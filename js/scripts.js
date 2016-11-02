c641aabb5ebad5461c339ab48e994f10a31d539e
Sergio Josh Villegas <joshviba_7@hotmail.com>
2016-11-02 14:20:50 -0700

    Its now A log cabin

--- js/scripts.js
+++ js/scripts.js
@@ -27,17 +27,32 @@ function drawBackground() {
     "use strict";
     shapes = document.getElementById("myShapes");
     ctx = shapes.getContext("2d");
-    ctx.fillStyle = "cyan";
+    ctx.fillStyle = "lightskyBlue";
     ctx.fillRect(0, 0, shapes.width, shapes.height);
-    ctx.fillStyle = "darkOliveGreen";
+    ctx.fillStyle = "green";
     ctx.fillRect(0, (bottomBackFaceHouseBodyY - houseDimensionY / 4), shapes.width, shapes.height);
+}
+
+function drawLogs() {
+    "use strict";
+    shapes = document.getElementById("myShapes");
+    ctx = shapes.getContext("2d");
+    var logs = houseDimensionY / 20;
+    var logsInterval = 20;
+    for (var i = 0; i < logs; i++) {
+        ctx.beginPath();
+        ctx.moveTo(rightFrontFaceHouseBodyX, bottomFrontFaceHouseBodyY - (i * logsInterval));
+        ctx.lineTo(rightBackFaceHouseBodyX, bottomBackFaceHouseBodyY - (i * logsInterval));
+        ctx.closePath();
+        ctx.stroke();
+    }
 }
 
 function houseBody() {
     "use strict";
     shapes = document.getElementById("myShapes");
     ctx = shapes.getContext("2d");
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "chocolate";
     ctx.lineWidth = 2;
     //Fills and outlines the "Back Face" of the house body
     ctx.fillRect(leftBackFaceHouseBodyX, topBackFaceHouseBodyY, houseDimensionX, houseDimensionY);
@@ -60,7 +75,9 @@ function houseBody() {
     ctx.lineTo(rightFrontFaceHouseBodyX, topFrontFaceHouseBodyY);
     ctx.closePath();
     ctx.stroke();
+    drawLogs();
     //Fills and outlines the "Front Face" of the house body
+    ctx.fillStyle = "red";
     ctx.fillRect(leftFrontFaceHouseBodyX, topFrontFaceHouseBodyY, houseDimensionX, houseDimensionY);
     ctx.strokeRect(leftFrontFaceHouseBodyX, topFrontFaceHouseBodyY, houseDimensionX, houseDimensionY);
 }
@@ -95,13 +112,51 @@ function drawDecorBody() {
     "use strict";
     shapes = document.getElementById("myShapes");
     ctx = shapes.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.lineWidth = 2;
+    ctx.fillStyle = "brown";
+    ctx.lineWidth = 1;
+    //Variables for door
     var doorDimensionX = houseDimensionX / 4;
     var doorDimensionY = houseDimensionY / 2;
-    var doptTopX = leftFrontFaceHouseBodyX + (houseDimensionX / 2 - doorDimensionX / 2);
-    var doptTopY = topFrontFaceHouseBodyY + (houseDimensionY / 2);
-    ctx.fillRect(doptTopX, doptTopY, doorDimensionX, doorDimensionY);
+    var doorTopX = leftFrontFaceHouseBodyX + (houseDimensionX / 2 - doorDimensionX / 2);
+    var doorTopY = topFrontFaceHouseBodyY + (houseDimensionY / 2);
+    //Varaibles for Door Knob
+    var knobRadius = 3;
+    var knobCenterX = doorTopX + doorDimensionX - knobRadius - 3;
+    var knobCenterY = doorTopY + doorDimensionY / 2;
+    var knobStartAngle = 0;
+    var knobEndAngle = 2 * Math.PI;
+    //Variables for windows
+    var windowSize = houseDimensionY / 3;
+    var windowTopY = topFrontFaceHouseBodyY + windowSize / 3;
+    var window1TopX = leftFrontFaceHouseBodyX + windowSize / 2;
+    var window2TopX = window1TopX + windowSize * 2.5;
+    //Draws the door and oulines
+    ctx.fillRect(doorTopX, doorTopY, doorDimensionX, doorDimensionY);
+    ctx.strokeRect(doorTopX, doorTopY, doorDimensionX, doorDimensionY);
+    //Draws the door knob
+    ctx.beginPath();
+    ctx.arc(knobCenterX, knobCenterY, knobRadius, knobStartAngle, knobEndAngle);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    //Sets the background for the windows
+    ctx.fillStyle = "gray";
+    //Draws and outlines the windows
+    ctx.fillRect(window1TopX, windowTopY, windowSize, windowSize);
+    ctx.strokeRect(window1TopX, windowTopY, windowSize, windowSize);
+    ctx.fillRect(window2TopX, windowTopY, windowSize, windowSize);
+    ctx.strokeRect(window2TopX, windowTopY, windowSize, windowSize);
+    //Draws the frames for the windows
+    ctx.beginPath();
+    ctx.moveTo(window1TopX + windowSize / 2, windowTopY);
+    ctx.lineTo(window1TopX + windowSize / 2, windowTopY + windowSize);
+    ctx.moveTo(window1TopX, windowTopY + windowSize / 2);
+    ctx.lineTo(window1TopX + windowSize, windowTopY + windowSize / 2);
+    ctx.moveTo(window2TopX + windowSize / 2, windowTopY);
+    ctx.lineTo(window2TopX + windowSize / 2, windowTopY + windowSize);
+    ctx.moveTo(window2TopX, windowTopY + windowSize / 2);
+    ctx.lineTo(window2TopX + windowSize, windowTopY + windowSize / 2);
+    ctx.closePath();
+    ctx.stroke();
 }
 
 function house() {
@@ -109,5 +164,5 @@ function house() {
     drawBackground();
     houseBody();
     houseRoof();
-    drawDecorBody()
+    drawDecorBody();
 }
