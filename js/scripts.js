d37eb8bf405af301fe8e6c02ec92423c5b70dd07
Sergio Josh Villegas <joshviba_7@hotmail.com>
2016-11-02 11:09:22 -0700

    Added a bagilion variables to draw the body of the house
    Drew the body of the house, and set variables for drawing the roof

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,69 @@
 var shapes;
 var ctx;
+//Variables for the house body
+var houseDimension = 160;
+var leftFrontFaceHouseBodyX = 100;
+var rightFrontFaceHouseBodyX = leftFrontFaceHouseBodyX + houseDimension;
+var topFrontFaceHouseBodyY = 300;
+var bottomFrontFaceHouseBodyY = topFrontFaceHouseBodyY + houseDimension;
+var leftBackFaceHouseBodyX = 230;
+var rightBackFaceHouseBodyX = leftBackFaceHouseBodyX + houseDimension;
+var topBackFaceHouseBodyY = 220;
+var bottomBackFaceHouseBodyY = topBackFaceHouseBodyY + houseDimension;
+//Variables for the house roof
+var leftFrontRoofCornerX = leftFrontFaceHouseBodyX - (houseDimension / 8);
+var rightFrontRoofCorner = rightFrontFaceHouseBodyX + (houseDimension / 8);
+var bottomFrontRoofY = topFrontFaceHouseBodyY;
+var pointFrontRoofX = leftFrontFaceHouseBodyX + (houseDimension / 2);
+var pointFrontRoofY = topFrontFaceHouseBodyX + (houseDimension / 3);
+var leftBackRoofCornerX = leftBackFaceHouseBodyX - (houseDimension / 8);
+var rightBackRoofCorner = rightBackFaceHouseBodyX + (houseDimension / 8);
+var bottomBackRoofY = topBackFaceHouseBodyY;
+var pointBackRoofX = leftBackFaceHouseBodyX + (houseDimension / 2);
+var pointBackRoofY = topBackFaceHouseBodyX + (houseDimension / 3);
 
-function house() {
+function houseBody() {
     "use strict";
     shapes = document.getElementById("myShapes");
     ctx = shapes.getContext("2d");
     ctx.fillStyle = "red";
     ctx.lineWidth = 2;
-    ctx.fillRect(230, 220, 160, 160);
-    ctx.strokeRect(230, 220, 160, 160);
+    //Fills and outlines the "Back Face" of the house body
+    ctx.fillRect(leftBackFaceHouseBodyX, topBackFaceHouseBodyY, houseDimension, houseDimension);
+    ctx.strokeRect(leftBackFaceHouseBodyX, topBackFaceHouseBodyY, houseDimension, houseDimension);
+    //Fills the space between the "faces" with colour
     ctx.beginPath();
-    ctx.moveTo(150, 300);
-    ctx.lineTo(230, 220);
-    ctx.lineTo(390, 380);
-    ctx.lineTo(310, 460);
+    ctx.moveTo(leftFrontFaceHouseBodyX, topFrontFaceHouseBodyY);
+    ctx.lineTo(leftBackFaceHouseBodyX, topBackFaceHouseBodyY);
+    ctx.lineTo(rightBackFaceHouseBodyX, bottomBackFaceHouseBodyY);
+    ctx.lineTo(rightFrontFaceHouseBodyX, bottomFrontFaceHouseBodyY);
     ctx.closePath();
     ctx.fill();
+    //Draws the lines connecting the corners to give appearance of depth
     ctx.beginPath();
-    ctx.moveTo(150, 300);
-    ctx.lineTo(230, 220);
-    ctx.moveTo(390, 380);
-    ctx.lineTo(310, 460);
-    ctx.moveTo(390, 220);
-    ctx.lineTo(310, 300);
+    ctx.moveTo(leftFrontFaceHouseBodyX, topFrontFaceHouseBodyY);
+    ctx.lineTo(leftBackFaceHouseBodyX, topBackFaceHouseBodyY);
+    ctx.moveTo(rightBackFaceHouseBodyX, bottomBackFaceHouseBodyY);
+    ctx.lineTo(rightFrontFaceHouseBodyX, bottomFrontFaceHouseBodyY);
+    ctx.moveTo(rightBackFaceHouseBodyX, topBackFaceHouseBodyY);
+    ctx.lineTo(rightFrontFaceHouseBodyX, topFrontFaceHouseBodyY);
     ctx.closePath();
     ctx.stroke();
-    ctx.fillRect(150, 300, 160, 160);
-    ctx.strokeRect(150, 300, 160, 160);
+    //Fills and outlines the "Front Face" of the house body
+    ctx.fillRect(leftFrontFaceHouseBodyX, topFrontFaceHouseBodyY, houseDimension, houseDimension);
+    ctx.strokeRect(leftFrontFaceHouseBodyX, topFrontFaceHouseBodyY, houseDimension, houseDimension);
+}
+
+function houseRoof() {
+    "use strict";
+    shapes = document.getElementById("myShapes");
+    ctx = shapes.getContext("2d");
+    ctx.fillStyle = "orange";
+    ctx.lineWidth = 2;
+}
+
+function house() {
+    "use strict";
+    houseBody();
+    houseRoof();
 }
